963f7909415f84a89b4f3b3c75a6e6a8b80d3fbb
Ursulino Costa <71571163+Ursulinocosta@users.noreply.github.com>
2021-09-18 15:53:00 -0300

    Add files via upload
    raio de um circulo

---
+++ 11/circunferencia.py
@@ -0,0 +1,5 @@
+raio = float(input('Entre com o valor do raio para obter a circunferencia do circulo: '))
+
+circunferencia = 2 * 3.14 * raio
+
+print("A circunferencia do circulo: {:.2f}".format(circunferencia))
